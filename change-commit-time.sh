c0492c86fdd92b2a0724aff61b53ebea9b493bf3
The Mariner <themarinerof@gmail.com>
2016-11-04 10:43:56 -0700

    updates

--- change-commit-time.sh
+++ change-commit-time.sh
@@ -4,9 +4,16 @@ echo $3
 
 # Fri Jan 2 21:38:53 2009 -0800
 
-git filter-branch --env-filter \
-    'if [ $GIT_COMMIT = $1 ]
-     then
-         export GIT_AUTHOR_DATE=$2
-         export GIT_COMMITTER_DATE=$3
-     fi'
+# git filter-branch --env-filter \
+#     'if [ $GIT_COMMIT = $1 ]
+#      then
+#          export GIT_AUTHOR_DATE=$2
+#          export GIT_COMMITTER_DATE=$3
+#      fi'
+
+ git filter-branch --env-filter \
+     'if [ $GIT_COMMIT = 256b746174d552a8f5cace5e61c297d7f8121ee6 ]
+      then
+          export GIT_AUTHOR_DATE="Fri Nov 2 10:39:19 2016 -0700"
+          export GIT_COMMITTER_DATE="Fri Nov 2 10:39:19 2016 -0700"
+      fi'
